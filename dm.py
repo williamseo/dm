ec38ff33c9ce27154e00ae746f84e7ab6b80c25c
William Seo <minwooks@gmail.com>
2021-06-07 12:26:20 +0900

    링크주소 변경

--- dm.py
+++ dm.py
@@ -8,12 +8,12 @@ chromedriver = 'D:\\webdriver\chromedriver.exe'
 # selenum의 webdriver에 앞서 설치한 chromedirver를 연동한다.
 driver = webdriver.Chrome(chromedriver)
 # driver로 특정 페이지를 크롤링한다.
-driver.get('https://auto.naver.com/bike/mainList.nhn')
+driver.get('https://www.gov.kr/nlogin/?curr_url=/mw%2FAA040OfferMainFrm.do%3Fcapp_biz_cd%3D15000000098%26tp_seq%3D')
 
 print("+" * 100)
 print(driver.title)   # 크롤링한 페이지의 title 정보
 print(driver.current_url)  # 현재 크롤링된 페이지의 url
-print("바이크 브랜드 크롤링")
+print("건축물대장 크롤링")
 print("-" * 100)
 
 
