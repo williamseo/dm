36894deca2113528316ce7a47c7bc779c669f9f6
William Seo <minwooks@gmail.com>
2021-06-08 16:38:33 +0900

    수정

---
+++ test.py
@@ -0,0 +1,9 @@
+#!/d/sysutils/conda/python.exe
+# -*- coding: utf-8 -*-
+
+f = open('address.txt','r',encoding='utf-8')
+while True:
+    line = f.readline()
+    if not line: break
+    elementsL = line.split('|')
+    print(elementsL[1],elementsL[2],elementsL[3],'%s-%s'%(elementsL[6],elementsL[7]))
